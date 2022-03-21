cb09b40b67b6e32b0665c92ae9be2b490fa929a3
JayMarBu <57606684+JayMarBu@users.noreply.github.com>
2022-03-21 13:20:19 +0000

    Setup for task 3&4

--- Mat400_Assignment.m
+++ Mat400_Assignment.m
@@ -11,6 +11,7 @@ disp('Mat400 Assignment');
 % - ? = (3,1,2) rads^-1
 % - t 0 -> 20 s
 
+% set values
 M = 10;
 r = 1;
 h = 4;
@@ -19,8 +20,10 @@ w_init = [3,1,2];
 t_max = 20;
 dt = 0.1;
 
+% solve for angular velocity
 [x y z t] = SolveRK4(M, r, h, w_init, t_max, dt);
 
+% plot results
 plot(t, x, 'r');
 hold on
 plot(t, y, 'b');
@@ -31,3 +34,11 @@ title('Task 1 & 2')
 xlabel('time (s)')
 ylabel('angular velocity (rads-1)')
 legend('x','y','z')
+
+% Task 2:
+% Solve the Semi-Implcit Euler equations of the cone  to obtrain its 
+% trajctory over the same time period using the following properties:
+% - u = (0,0,200)ms-1
+% - a = (0,0,-9.8)ms-2
+% - t 0 -> 20 s
+
---
+++ SolveSemiImplicitEuler.m
@@ -0,0 +1,7 @@
+function [x y z t] = SolveSemiImplicitEuler( M, u, a, t_max, dt )
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
